ec77f6359dc413790254999f9dbc194aeece95e5
Anibal Dams <75810272+AnibalDams@users.noreply.github.com>
2024-09-02 13:51:24 -0400

    Update SavedCheck.svelte

--- src/components/SavedCheck.svelte
+++ src/components/SavedCheck.svelte
@@ -1,8 +1,5 @@
-<script>
 
-</script>
-
-<button  class="ship">Saved</button>
+<button  class="ship" >‎ ‎ <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#002202"><path d="M240-144v-600q0-30 21-51t51-21h336q30 0 51 21t21 51v600l-240-96-240 96Z"/></svg></button>
 
 
 
@@ -10,8 +7,9 @@
 
 <style>
     .ship{
+        position: relative;
         margin-bottom:5px;
-        background-color: #01aa0a;
+        background-color: #02cc0d;
         padding-left: 10px;
         padding-right: 10px;
         padding-top: 5px;
@@ -23,6 +21,11 @@
         font-size: 16px;
         font-weight: bold;
         transition: 0.1s;
+
     }
+svg{
+    position: absolute;
+    left: 2px;
+}
 
 </style>
