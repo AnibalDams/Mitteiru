b0ab552df2b91166bd02120cef94252453449e6c
Anibal Dams <75810272+AnibalDams@users.noreply.github.com>
2024-10-16 03:05:36 -0400

    Adding mobile page for the anime details page

--- src/routes/anime/[animeId]/+page.svelte
+++ src/routes/anime/[animeId]/+page.svelte
@@ -1,61 +1,102 @@
 <script>
-    import { onMount } from "svelte";
-    import { goto } from "$app/navigation";
-    import Header from "../../Header.svelte";
-    import AnimePage from "./components/AnimePage.svelte";
-    import NotFoundError from "../../../components/NotFoundError.svelte";
-    import axios from "axios";
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  import Header from "../../Header.svelte";
+  import AnimePage from "./components/AnimePage.svelte";
+  import NotFoundError from "../../../components/NotFoundError.svelte";
+  import axios from "axios";
+  import MobileHeader from "../../../components/MobileHeader.svelte";
+  import MobilePage from "./components/MobilePage.svelte";
 
-    export let data;
+  export let data;
 
-    let profileId = "";
-    let profileImage = "";
-    let profileName = "";
-    let logged = "";
-    let animesInList = []
-    let profileLists= []
-    let likesCount = 0;
-    let profileLikes = [];
-    onMount(async () => {
-        const userId = data.userId;
-        if (userId && userId.length > 0) {
-            const { getCookie } = await import("svelte-cookie");
-            profileId = getCookie("profileId");
-            profileImage = getCookie("profileImage");
-            profileName = getCookie("profileName");
-            if (profileId.length <= 0) {
-                goto("/selectprofile");
-            }
-            let getLists = await axios(
-				`https://mitteiru-backend.onrender.com/user/profile/${profileId}/list/all`,
-			);
-          
-            let getAnimesInList = await axios(
-				`https://mitteiru-backend.onrender.com/user/profile/${profileId}/list/anime/all`,
-			);
-            let getLikes = (await axios(`https://mitteiru-backend.onrender.com/anime/${data.anime.id}/likes/count`))
-            likesCount = getLikes.data.likesCount
-            profileLikes = getLikes.data.profiles
-            await axios.post(`https://mitteiru-backend.onrender.com/user/profile/${profileId}/history/${data.anime.id}/0/add`)
-            profileLists = getLists.data.lists
-            animesInList = getAnimesInList.data.animes
-            logged = "si";
-        } else {
-            logged = "no";
-        }
-    });
+  let profileId = "";
+  let profileImage = "";
+  let profileName = "";
+  let logged = "";
+  let animesInList = [];
+  let profileLists = [];
+  let likesCount = 0;
+  let profileLikes = [];
+  let isMobile;
+  onMount(async () => {
+    const userId = data.userId;
+    if (userId && userId.length > 0) {
+      const { getCookie } = await import("svelte-cookie");
+      profileId = getCookie("profileId");
+      profileImage = getCookie("profileImage");
+      profileName = getCookie("profileName");
+      if (profileId.length <= 0) {
+        goto("/selectprofile");
+      }
+      let getLists = await axios(
+        `https://mitteiru-backend.onrender.com/user/profile/${profileId}/list/all`
+      );
+
+      let getAnimesInList = await axios(
+        `https://mitteiru-backend.onrender.com/user/profile/${profileId}/list/anime/all`
+      );
+      let getLikes = await axios(
+        `https://mitteiru-backend.onrender.com/anime/${data.anime.id}/likes/count`
+      );
+      likesCount = getLikes.data.likesCount;
+      profileLikes = getLikes.data.profiles;
+      await axios.post(
+        `https://mitteiru-backend.onrender.com/user/profile/${profileId}/history/${data.anime.id}/0/add`
+      );
+      profileLists = getLists.data.lists;
+      animesInList = getAnimesInList.data.animes;
+      logged = "si";
+    } else {
+      logged = "no";
+    }
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    isMobile = mediaQuery.matches;
+
+    const handleResize = () => {
+      isMobile = mediaQuery.matches;
+    };
+
+    mediaQuery.addEventListener("change", handleResize);
+    console.log(isMobile)
+    return () => {
+      mediaQuery.removeEventListener("change", handleResize);
+    };
+  });
 </script>
 
 <svelte:head>
-    <title>{!data.anime ? "Anime not found" : data.anime.name?data.anime.name:"Loading..."}</title>
-    <meta name="description" content="Svelte demo app" />
+  <title
+    >{!data.anime
+      ? "Anime not found"
+      : data.anime.name
+        ? data.anime.name
+        : "Loading..."}</title
+  >
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<Header {logged} {profileImage} name={profileName} />
-{#if !data.anime}
-    <NotFoundError
-        text="This anime wasn't found. Are you sure that you put the right anime in the url? Check it!"
-    />
+{#if isMobile}
+    <MobileHeader/>
 {:else}
-    <AnimePage dataA={data} {logged} {profileId} {profileLists} {animesInList} likesCount={likesCount} profileLikes={profileLikes}/>
+<Header {logged} {profileImage} name={profileName} />
+{/if}
+{#if !data.anime}
+  <NotFoundError
+    text="This anime wasn't found. Are you sure that you put the right anime in the url? Check it!"
+  />
+{:else}
+{#if isMobile}
+<MobilePage data={data}/>
+{:else}
+<AnimePage
+dataA={data}
+{logged}
+{profileId}
+{profileLists}
+{animesInList}
+{likesCount}
+{profileLikes}
+/>
+{/if}
 {/if}
---
+++ src/routes/anime/[animeId]/components/MobilePage.svelte
@@ -0,0 +1,75 @@
+
+<script>
+    export let data;
+</script>
+
+
+
+
+<div class="cover_container" 
+
+style={`background-image: linear-gradient(to top, #fff, transparent),linear-gradient(to bottom, #fff, transparent),${data.anime.studio==="animeFlv"?"url("+data.anime.cover+")":"url("+"https://mitteiru-backend.onrender.com/static/"+data.anime.image+")"};`}
+
+>
+    <img src={data.anime.cover} alt="">
+    <div class="title_container">
+        <span class="name">{data.anime.name}</span>
+        <span class="japanese_name">{data.anime.japanese_name}</span>
+    </div>
+</div>
+<p>{data.anime.synopsis}</p>
+
+
+
+<style>
+    div.cover_container{
+        width: 100%;
+        height: 500px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        flex-direction: column;
+
+    }
+    div.cover_container img{
+        width: 200px;
+        height: 300px;
+        object-fit: cover;
+        border-radius: 10px;
+        /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); */
+    }
+    div.title_container{
+        margin-top: 20px;
+        padding-left: 20px;
+        padding-right: 20px;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+    }
+    div.title_container span.name{
+        font-size: 25px;
+        font-weight: bold;
+    }
+    div.title_container span.japanese_name{
+        font-size: 15px;
+        font-weight: bold;
+    }
+    p{
+        padding-left: 20px;
+        padding-right: 20px;
+        text-align: justify;
+        line-height: 1.2;
+        font-size: 18px;
+        font-weight: lighter;
+        max-width: 800px;
+        margin-bottom: 40px;
+    }
+</style>
+
+
