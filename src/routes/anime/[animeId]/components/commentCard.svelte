1975d6767c222b90d5993c133713013700726ffe
Anibal Dams <75810272+AnibalDams@users.noreply.github.com>
2025-03-18 11:57:00 -0400

    Update README.md

--- src/routes/anime/[animeId]/components/commentCard.svelte
+++ src/routes/anime/[animeId]/components/commentCard.svelte
@@ -1,79 +1,111 @@
 <script>
-    import {} from 'radix-icons-svelte'
 </script>
 
 <div class="comment">
-    <div class="right">
-        <img src="https://i.ibb.co/b7XFdQC/59afdf34673fcfaf6b22272229b122d9.png" alt="" class="avatar">
+  <div class="right">
+    <img
+      src="https://i.ibb.co/b7XFdQC/59afdf34673fcfaf6b22272229b122d9.png"
+      alt=""
+      class="avatar"
+    />
+  </div>
+  <div class="left">
+    <div class="top">
+      <span class="name">Anibal Dams</span>
+      <span class="date">1 day ago</span>
     </div>
-    <div class="left">
-        <div class="top">
-            <span class="name">Anibal Dams</span>
-            <span class="date">1 day ago</span>
-        </div>
-        <div class="bottom">
-            <p class="text">Este episodio fue simplemente increíble. La narrativa alcanzó su punto más alto con giros inesperados que me mantuvieron pegado a la pantalla de principio a fin. La animación fue impecable, especialmente en las escenas de acción, donde cada movimiento estaba cargado de dinamismo y emoción. Además, los personajes mostraron una profundidad increíble, y los momentos de tensión emocional realmente conectaron. Sin duda, uno de los mejores episodios de toda la serie. ¡Totalmente imperdible!</p>
-            <div class="actions">
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" style="margin-right: 5px;" fill="#1f1f1f"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/></svg>
-                    10
-                </button>
-                <button><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" style="margin-right: 5px;" fill="#1f1f1f"><path d="M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z"/></svg> 50</button>
-            </div>
-        </div>
+    <div class="bottom">
+      <p class="text">
+        Este episodio fue simplemente increíble. La narrativa alcanzó su punto
+        más alto con giros inesperados que me mantuvieron pegado a la pantalla
+        de principio a fin. La animación fue impecable, especialmente en las
+        escenas de acción, donde cada movimiento estaba cargado de dinamismo y
+        emoción. Además, los personajes mostraron una profundidad increíble, y
+        los momentos de tensión emocional realmente conectaron. Sin duda, uno de
+        los mejores episodios de toda la serie. ¡Totalmente imperdible!
+      </p>
+      <div class="actions">
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20px"
+            viewBox="0 -960 960 960"
+            width="20px"
+            style="margin-right: 5px;"
+            fill="#1f1f1f"
+            ><path
+              d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"
+            /></svg
+          >
+          10
+        </button>
+        <button
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20px"
+            viewBox="0 -960 960 960"
+            width="20px"
+            style="margin-right: 5px;"
+            fill="#1f1f1f"
+            ><path
+              d="M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z"
+            /></svg
+          > 50</button
+        >
+      </div>
     </div>
+  </div>
 </div>
 
-
 <style>
-    .comment{
-        display: flex;
-        margin: 10px;
-        border: 1px solid #c4c3c3;
-        border-radius: 10px;
-        padding:10px;
-    }
-    .comment .right{
-        margin-right: 20px;
-    }
-    .comment .right img{
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        object-fit: cover;
-    }
-    .comment .left .top{
-        display: flex;
-        margin-bottom: 10px;
-    }
-    .comment .left .top .name{
-        display: inline-block;
-        font-weight: bold;
-        margin-right: 10px;
-    }
-    .comment .left .top .date{
-        color:grey;
-    }
-    .comment .left .bottom .actions{
-        margin-top: 10px;
-        display: flex;
-        align-items: center;
-        justify-content: flex-start;
-
-    }
-    button{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 5px;
-        border-radius: 5px;
-        background: none;
-        border: none;
-        margin-right: 10px;
-        font-weight: bold;
-        cursor: pointer;
-    }
-    button:hover{
-        background-color: rgba(218, 218, 218, 0.534);
-    }
+  .comment {
+    display: flex;
+    margin: 10px;
+    min-height: 150px;
+    border: 1px solid #c4c3c3;
+    border-radius: 10px;
+    padding: 10px;
+  }
+  .comment .right {
+    margin-right: 20px;
+  }
+  .comment .right img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+  .comment .left .top {
+    display: flex;
+    margin-bottom: 10px;
+  }
+  .comment .left .top .name {
+    display: inline-block;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+  .comment .left .top .date {
+    color: grey;
+  }
+  .comment .left .bottom .actions {
+    margin-top: 10px;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-start;
+  }
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px;
+    border-radius: 5px;
+    background: none;
+    border: none;
+    margin-right: 10px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: rgba(218, 218, 218, 0.534);
+  }
 </style>
