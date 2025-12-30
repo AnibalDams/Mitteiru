db332260d30be5e227b2252ac66b65c9057bf08d
Aníbal Dams <elnini120@gmail.com>
2025-12-30 11:37:08 -0400

    Update MobilePage.svelte

--- src/routes/user/profile/history/components/MobilePage.svelte
+++ src/routes/user/profile/history/components/MobilePage.svelte
@@ -14,44 +14,58 @@
 
 <MobileHeader {logged} {profileName} {profileImage} />
 
-    <div class="container">
-        {#if history.length > 0}
-            {#each history as h}
-                <span>{h.date == today ? "Today" : h.date}</span>
-                <div class="animes_container">
-                    {#each h.animes as anime}
-                        <MobileAnimeCard
-                            cover={anime.cover}
-                            title={anime.name}
-                            id={anime._id}
-                        />
-                    {/each}
-                </div>
-            {/each}
-        {:else}
-            <div class="no_history">
-                <img
-                    src={`${PUBLIC_API_URL}/static/WhatsApp Image 2025-01-15 at 3.18.19 PM.jpeg`}
-                    alt=""
-                />
-                <span>You haven't visited an anime yet</span>
+<div class="container">
+    {#if history.length > 0}
+        {#each history as h}
+            <span>{h.date == today ? "Today" : h.date}</span>
+            <div class="animes_container">
+                {#each h.animes as anime}
+                    <MobileAnimeCard
+                        cover={anime.cover}
+                        title={anime.name}
+                        id={anime._id}
+                    />
+                {/each}
             </div>
-        {/if}
-    </div>
-
+        {/each}
+    {:else}
+        <div class="no_history">
+            <img
+                src={`${PUBLIC_API_URL}/static/WhatsApp Image 2025-01-15 at 3.18.19 PM.jpeg`}
+                alt=""
+            />
+            <span>You haven't visited an anime yet</span>
+        </div>
+    {/if}
+</div>
 
 <style>
-    .container{
-        margin:12px;
+    .container {
+        margin: 12px;
     }
-    .container span{
-        font-size:20px;
-        font-weight:bold;
+    .container span {
+        font-size: 20px;
+        font-weight: bold;
     }
     .animes_container {
         margin-top: 30px;
         display: flex;
         overflow: auto;
         white-space: nowrap;
+    }
+    .no_history {
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, 50%);
+    }
+    .no_history img {
+        width: 200px;
+        height: 200px;
+        object-fit: cover;
     }
 </style>
